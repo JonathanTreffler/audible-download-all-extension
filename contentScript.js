1f9ec534e89df84785980ff89acd052c20ee392c
Jonathan Treffler <mail@jonathan-treffler.de>
2021-01-17 11:49:11 +0000

    modified content script

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,4 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
+let delay = ms => new Promise(res => setTimeout(res, ms));
 
 async function pressAllButtons() {
     let buttons = $("#adbl-library-content-row-B08K32V928 > div.bc-row-responsive.bc-spacing-top-s2 > div.bc-col-responsive.bc-spacing-top-none.bc-col-10 > div > div.bc-col-responsive.adbl-library-action.bc-col-2.bc-col-offset-1 > div:nth-child(3) > span > a > span > span");
